aad0449216ff84bbae65c3c79fe68dc30bc421d7
Mineli <monte.nemi@gmail.com>
2020-03-02 09:55:10 +0100

    uploading to github

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Ajax PHP</title>
+  <style>
+  #tables {
+  display: flex;
+  }
+  </style>
+
+</head>
+<body>
+  <form action="" id="formId" method="POST">
+    <input type="checkbox" name="checkbox[]" value="1">1
+    <input type="checkbox" name="checkbox[]" value="2">2
+    <input type="checkbox" name="checkbox[]" value="3">3
+    <input type="checkbox" name="checkbox[]" value="4">4
+    <input type="checkbox" name="checkbox[]" value="5">5
+    <input type="submit" name="submit" value="Submit">
+  </form>
+
+  <div id="table"></div>
+
+<script>
+    let form = document.getElementById("formId");
+    let table = document.getElementById("table");
+
+    form.addEventListener('submit', function(e) {
+      e.preventDefault();
+      let formData = new FormData(form);
+
+      let xhr = new XMLHttpRequest();
+      xhr.open ('POST', 'process.php', true);
+      xhr.onload = function() {
+        let result = '';
+        result = this.responseText;
+        table.innerHTML = result;
+        }
+      xhr.send(formData);
+    });
+  </script>
+</body>
+</html>
+
---
+++ process.php
@@ -0,0 +1,17 @@
+<?php
+  if(isset($_POST['checkbox'])) {
+  $checkbox = $_POST['checkbox'];
+
+  foreach($checkbox as $value) {
+    ?>
+    <div id="tables">
+    <?php
+    for($i = 1; $i <= 10; $i++) {
+      echo ($value . " * " . $i . " = " . (intval($value) * $i) . "<br>");
+    }
+  }
+  ?>
+    </div>
+  <?php
+  }
+  ?>
