365780797b301d7c05e7e62a128c38ec57bb7b2b
Mineli <monte.nemi@gmail.com>
2020-03-02 11:03:41 +0100

    updated css

--- index.php
+++ index.php
@@ -12,12 +12,17 @@
 </head>
 <body>
   <form action="" id="formId" class="text-center" method="POST">
-    <input type="checkbox" name="checkbox[]" value="1">1
-    <input type="checkbox" name="checkbox[]" value="2">2
-    <input type="checkbox" name="checkbox[]" value="3">3
-    <input type="checkbox" name="checkbox[]" value="4">4
-    <input type="checkbox" name="checkbox[]" value="5">5
-    <input type="submit" name="submit" value="Submit">
+    <input type="checkbox" name="checkbox[]" value="1"><label>1</label>
+    <input type="checkbox" name="checkbox[]" value="2"><label>2</label>
+    <input type="checkbox" name="checkbox[]" value="3"><label>3</label>
+    <input type="checkbox" name="checkbox[]" value="4"><label>4</label>
+    <input type="checkbox" name="checkbox[]" value="5"><label>5</label>
+    <input type="checkbox" name="checkbox[]" value="6"><label>6</label>
+    <input type="checkbox" name="checkbox[]" value="7"><label>7</label>
+    <input type="checkbox" name="checkbox[]" value="8"><label>8</label>
+    <input type="checkbox" name="checkbox[]" value="9"><label>9</label>
+    <input type="checkbox" name="checkbox[]" value="10"><label>10</label>
+    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
   </form>
 
   <div id="tables">
