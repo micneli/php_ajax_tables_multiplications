d166fd0531ace66476e5f29e26cdcb815b8093cb
Mineli <monte.nemi@gmail.com>
2020-03-02 09:56:50 +0100

    indentation corrected

--- process.php
+++ process.php
@@ -1,17 +1,17 @@
 <?php
-  if(isset($_POST['checkbox'])) {
+if (isset($_POST['checkbox'])) {
   $checkbox = $_POST['checkbox'];
 
-  foreach($checkbox as $value) {
-    ?>
+  foreach ($checkbox as $value) {
+?>
     <div id="tables">
     <?php
-    for($i = 1; $i <= 10; $i++) {
+    for ($i = 1; $i <= 10; $i++) {
       echo ($value . " * " . $i . " = " . (intval($value) * $i) . "<br>");
     }
   }
-  ?>
+    ?>
     </div>
   <?php
-  }
+}
   ?>
